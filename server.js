149d3e6b7c79a0bbfb964e0a2e860759774542f2
Pa Yeng Thao <payengt24@gmail.com>
2018-04-17 11:52:33 -0500

    add request

--- server.js
+++ server.js
@@ -3,4 +3,18 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT);
+
+//req is request, res is response
+app.get('/', function (req, res) {
+    res.send('Hello World');
+    
+});
+
+app.get('/hey', function (req, res) {
+    res.send('Howdy');
+    
+});
+
+app.listen(PORT, function () {
+    console.log(`listening on port: ${PORT}`);
+});
